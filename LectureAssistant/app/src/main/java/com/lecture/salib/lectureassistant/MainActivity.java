7a0e613539d85badb1294ef253ccd315ea2cad1e
DanSalib <dansalib@outlook.com>
2018-05-18 22:41:40 -0400

    setup new activity to be used for speech detection

--- LectureAssistant/app/src/main/java/com/lecture/salib/lectureassistant/MainActivity.java
+++ LectureAssistant/app/src/main/java/com/lecture/salib/lectureassistant/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -22,34 +23,10 @@ public class MainActivity extends Activity {
     public static final String EXTRA_DATA = "com.lecture.salib.lectureassistant.EXTRA_DATA";
     private TextView mTextMessage;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-//            switch (item.getItemId()) {
-//                case R.id.navigation_home:
-//                    mTextMessage.setText(R.string.title_home);
-//                    return true;
-//                case R.id.navigation_dashboard:
-//                    mTextMessage.setText(R.string.title_dashboard);
-//                    return true;
-//                case R.id.navigation_notifications:
-//                    mTextMessage.setText(R.string.title_notifications);
-//                    return true;
-//            }
-            return false;
-        }
-
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
         String[] items = {"ali", "charbel", "heba", "andrew"};
         ListAdapter lectureAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
@@ -61,18 +38,34 @@ public class MainActivity extends Activity {
                 @Override
                 public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                     String item = String.valueOf(parent.getItemAtPosition(position));
-                    openActivity2(item);
+                    openListDataActivity(item);
                     //Toast.makeText(MainActivity.this, item, Toast.LENGTH_LONG).show();
                 }
             }
         );
+
+        Button listenButton = (Button) findViewById(R.id.listenButton);
+
+        listenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSpeechActivity();
+            }
+        });
     }
 
-    public void openActivity2(String text) {
+    public void openListDataActivity(String text) {
         Intent intent = new Intent(this, Main2Activity.class);
         intent.putExtra(EXTRA_DATA, text);
 
         startActivity(intent);
     }
+
+    public void openSpeechActivity() {
+        Intent intent = new Intent(this, speechActivity.class);
+
+        startActivity(intent);
+    }
+
 
 }
