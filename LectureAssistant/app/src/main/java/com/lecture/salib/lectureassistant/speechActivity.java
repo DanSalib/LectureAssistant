85a4ef55b5f35d403bface29298df3b4e115b29a
DanSalib <dansalib@outlook.com>
2018-05-18 23:50:25 -0400

    implemented android speech recognizer

--- LectureAssistant/app/src/main/java/com/lecture/salib/lectureassistant/speechActivity.java
+++ LectureAssistant/app/src/main/java/com/lecture/salib/lectureassistant/speechActivity.java
@@ -1,13 +1,61 @@
 package com.lecture.salib.lectureassistant;
 
+import android.content.Intent;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class speechActivity extends AppCompatActivity {
+
+    TextView speechText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_speech);
+
+        speechText = (TextView) findViewById(R.id.speechText);
     }
+
+    public void getSpeechInput(View view) {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PARTIAL_RESULTS, true);
+        intent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_POSSIBLY_COMPLETE_SILENCE_LENGTH_MILLIS, 10000);
+        intent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_COMPLETE_SILENCE_LENGTH_MILLIS, 10000);
+
+
+        if(intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, 10);
+        } else {
+            Toast.makeText(this, "Your Device Doesn't Support Speech Input", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case 10:
+                if(resultCode == RESULT_OK && data != null) {
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    speechText.setText(speechText.getText()+ " " + result.get(0));
+                } else if(resultCode == RESULT_CANCELED) {
+                    //ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_PARTIAL_RESULTS);
+                    //speechText.setText("cancelled");
+                }
+                break;
+        }
+    }
+
 }
